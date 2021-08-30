2533af3998c3634fc45418a869aee3afb3d27b74
John Paul Ong <jpong5202@gmail.com>
2021-08-30 09:53:24 +0800

    Add: global theme for mui

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,23 @@
 import "../styles/globals.scss";
+import { createTheme, ThemeProvider, CssBaseline } from "@material-ui/core";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#4461ca",
+    },
+    secondary: {
+      main: "#f44336",
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
