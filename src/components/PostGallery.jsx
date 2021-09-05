59805ce50bf3173e91fd653380dd10f00da774a8
John Paul Ong <jpong5202@gmail.com>
2021-09-05 13:32:35 +0800

    Add: placeholder images

--- src/components/PostGallery.jsx
+++ src/components/PostGallery.jsx
@@ -19,6 +19,8 @@ const PostGallery = () => {
               layout="fill"
               objectFit="cover"
               alt={post?.title}
+              placeholder="blur"
+              blurDataURL="/img/blur.jpg"
             />
             <div className="card-info">
               <div className="card-info-group">
--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,3 +1,27 @@
+import Head from "next/head";
+import NavBar from "components/NavBar";
+import FilterPanel from "components/FilterPanel";
+import PostGallery from "components/PostGallery";
+import SiteFooter from "components/SiteFooter";
+
 export default function Home() {
-  return <>Profile</>;
+  return (
+    <>
+      <Head>
+        <title>iFLEXHIBIT | Profile</title>
+        <meta
+          name="description"
+          content="iFLEXHIBIT is a content-sharing platform for iACADEMY students."
+        />
+      </Head>
+      <NavBar />
+      <main>
+        <div className="home-container">
+          <FilterPanel />
+          <PostGallery />
+        </div>
+      </main>
+      <SiteFooter />
+    </>
+  );
 }
