52e549a85c62c2411acbfccd75ddf4d892f2ca48
John Paul Ong <jpong5202@gmail.com>
2021-09-18 23:40:18 +0800

    Add gallery filter + sort controls

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,28 @@
+import Icon from "components/Icon";
+import IconButton from "components/IconButton";
 import Layout from "components/Layout";
+import Select from "components/Select";
+import styles from "styles/HomePage.module.scss";
+import { useState } from "react";
 
 export default function Home() {
+  const sortOptions = ["most viewed", "newest", "most popular"];
+  const [activeSort, setActiveSort] = useState(sortOptions[0]);
+  const handleSortChange = (e) => setActiveSort(e.target.value);
   return (
     <Layout
       title="iFLEXHIBIT"
       description="A content sharing platform for iACADEMY students"
       canonical="https://iflexhibit.com/"
-    ></Layout>
+    >
+      <div className={`${styles["controls"]}`}>
+        <IconButton icon={<Icon icon="filter" />} />
+        <Select
+          options={sortOptions}
+          value={activeSort}
+          onChange={handleSortChange}
+        />
+      </div>
+    </Layout>
   );
 }
