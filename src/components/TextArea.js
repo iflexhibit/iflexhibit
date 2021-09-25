caaa22cbe6a0e6d30b3d1a5aebf37fcb9e9ff841
John Paul Ong <jpong5202@gmail.com>
2021-09-25 22:10:34 +0800

    Add TextArea component and style

---
+++ src/components/TextArea.js
@@ -0,0 +1,22 @@
+import styles from "styles/TextArea.module.scss";
+
+const TextArea = ({ id, value, onChange, placeholder }) => {
+  const handleResize = (e) => {
+    if (e.target.scrollHeight >= 136) return;
+    e.target.style.height = "";
+    e.target.style.height = e.target.scrollHeight + "px";
+  };
+  return (
+    <textarea
+      className={styles["textarea"]}
+      onInput={handleResize}
+      id={id}
+      name={id}
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+    />
+  );
+};
+
+export default TextArea;
