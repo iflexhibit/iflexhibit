303e282531df788c002cb5fc4e13a09b8c2ce8df
John Paul Ong <jpong5202@gmail.com>
2021-09-27 00:55:30 +0800

    Update props list and add fullHeight bool

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -1,6 +1,14 @@
 import styles from "styles/TextArea.module.scss";
+import PropTypes from "prop-types";
 
-const TextArea = ({ id, value, onChange, placeholder, autoFocus }) => {
+const TextArea = ({
+  id,
+  value,
+  onChange,
+  placeholder,
+  autoFocus,
+  fullHeight,
+}) => {
   const handleResize = (e) => {
     if (e.target.scrollHeight >= 136) return;
     e.target.style.height = "";
@@ -8,7 +16,9 @@ const TextArea = ({ id, value, onChange, placeholder, autoFocus }) => {
   };
   return (
     <textarea
-      className={styles["textarea"]}
+      className={`${styles["textarea"]} ${
+        fullHeight ? styles["fullHeight"] : ""
+      }`}
       onInput={handleResize}
       id={id}
       name={id}
@@ -18,6 +28,24 @@ const TextArea = ({ id, value, onChange, placeholder, autoFocus }) => {
       autoFocus={autoFocus}
     />
   );
+};
+
+TextArea.propTypes = {
+  id: PropTypes.string,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  onChange: PropTypes.func,
+  placeholder: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  autoFocus: PropTypes.bool,
+  fullHeight: PropTypes.bool,
+};
+
+TextArea.defaultProps = {
+  id: "",
+  value: "",
+  onChange: () => {},
+  placeholder: "",
+  autoFocus: false,
+  fullHeight: false,
 };
 
 export default TextArea;
