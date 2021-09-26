559898b66eb8bf6f1318a0b4d614f3736ef16db8
John Paul Ong <jpong5202@gmail.com>
2021-09-26 16:45:53 +0800

    Add link to profile in bottom nav

--- src/components/NavBottom.js
+++ src/components/NavBottom.js
@@ -9,7 +9,7 @@ const NavBottom = () => {
     <nav className={`${styles["nav"]} ${styles["bottom"]}`}>
       <IconButton icon={<HomeIcon />} fullWidth fullHeight href="/" />
       <IconButton icon={<PlusIcon />} variant="outlined" rounded />
-      <IconButton icon={<UserIcon />} fullWidth fullHeight />
+      <IconButton icon={<UserIcon />} fullWidth fullHeight href="/profile" />
     </nav>
   );
 };
