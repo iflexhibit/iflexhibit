2cf127c0ba4c5ccd1fcbf00b0abc79e05dda521e
John Paul Ong <jpong5202@gmail.com>
2021-10-02 18:33:24 +0800

    Separate Profile Section

--- src/components/layouts/AccountLayout.js
+++ src/components/layouts/AccountLayout.js
@@ -5,9 +5,9 @@ import ButtonGroup from "components/ButtonGroup";
 import Layout from "components/Layout.js";
 import Button from "components/Button";
 import IconButton from "components/IconButton";
-import TrashIcon from "components/icons/TrashIcon";
 import TextInput from "components/TextInput";
 import TextArea from "components/TextArea";
+import TrashIcon from "components/icons/TrashIcon";
 import RedoIcon from "components/icons/RedoIcon";
 
 const AccountLayout = () => {
@@ -82,99 +82,109 @@ const AccountLayout = () => {
             setActiveTab={setActiveTab}
           />
         </div>
-        <div className={`${styles["row"]} ${styles["avatar"]}`}>
-          <label>Profile Photo</label>
-          <div className={`${styles["image"]}`}>
-            <Image src={user?.avatarImg} layout="fill" objectFit="cover" />
-          </div>
-          <div className={`${styles["avatar-controls"]}`}>
-            <IconButton icon={<TrashIcon />} variant="warning" rounded />
-            <Button
-              label="Upload a photo"
-              fullWidth
-              variant="outlined"
-              rounded
-            />
-          </div>
-        </div>
-        <div className={`${styles["row"]} ${styles["banner"]}`}>
-          <label>Profile Banner</label>
-          <div className={`${styles["image"]}`}>
-            <Image src={user?.bannerImg} layout="fill" objectFit="cover" />
-          </div>
-          <div className={`${styles["banner-controls"]}`}>
-            <IconButton icon={<TrashIcon />} variant="warning" rounded />
-            <Button
-              label="Upload a photo"
-              fullWidth
-              variant="outlined"
-              rounded
-            />
-          </div>
-        </div>
-        <div className={`${styles["row"]} ${styles["default"]}`}>
-          <div className={styles["group"]}>
-            <label>First Name</label>
-            <span>{user?.realName?.firstName}</span>
-          </div>
-          <div className={styles["group"]}>
-            <label>Last Name</label>
-            <span>{user?.realName?.lastName}</span>
-          </div>
-          <div className={styles["group"]}>
-            <label>Email</label>
-            <span>{user?.email}</span>
-          </div>
-        </div>
-        <div className={`${styles["row"]} ${styles["form"]}`}>
-          <form onReset={handleFormReset} onSubmit={handleFormSubmit}>
-            <div className={styles["group"]}>
-              <label>Display Name</label>
-              <TextInput
-                id="displayName"
-                value={newProfile.displayName}
-                onChange={handleProfileChange}
-                placeholder={user?.displayName}
-              />
-            </div>
-            <div className={styles["group"]}>
-              <label>Contact Number</label>
-              <TextInput
-                id="contact"
-                value={newProfile.contact}
-                onChange={handleProfileChange}
-                placeholder={user?.contact}
-              />
-            </div>
-            <div className={styles["group"]}>
-              <label>Bio</label>
-              <TextArea
-                id="bio"
-                value={newProfile.bio}
-                onChange={handleProfileChange}
-                placeholder={user?.bio}
-                fullHeight
-              />
-            </div>
-            <div className={styles["form-controls"]}>
-              <IconButton
-                icon={<RedoIcon />}
-                type="reset"
-                variant="outlined"
-                rounded
-              />
-              <Button
-                type="submit"
-                label="save changes"
-                variant="contained"
-                rounded
-                fullWidth
-              />
-            </div>
-          </form>
-        </div>
+        <ProfileSection
+          user={user}
+          handleFormReset={handleFormReset}
+          handleFormSubmit={handleFormSubmit}
+          newProfile={newProfile}
+          handleProfileChange={handleProfileChange}
+        />
       </div>
     </Layout>
+  );
+};
+
+const ProfileSection = ({
+  user,
+  handleFormReset,
+  handleFormSubmit,
+  newProfile,
+  handleProfileChange,
+}) => {
+  return (
+    <div className={styles["profile"]}>
+      <div className={`${styles["row"]} ${styles["avatar"]}`}>
+        <label>Profile Photo</label>
+        <div className={`${styles["image"]}`}>
+          <Image src={user?.avatarImg} layout="fill" objectFit="cover" />
+        </div>
+        <div className={`${styles["avatar-controls"]}`}>
+          <IconButton icon={<TrashIcon />} variant="warning" rounded />
+          <Button label="Upload a photo" fullWidth variant="outlined" rounded />
+        </div>
+      </div>
+      <div className={`${styles["row"]} ${styles["banner"]}`}>
+        <label>Profile Banner</label>
+        <div className={`${styles["image"]}`}>
+          <Image src={user?.bannerImg} layout="fill" objectFit="cover" />
+        </div>
+        <div className={`${styles["banner-controls"]}`}>
+          <IconButton icon={<TrashIcon />} variant="warning" rounded />
+          <Button label="Upload a photo" fullWidth variant="outlined" rounded />
+        </div>
+      </div>
+      <div className={`${styles["row"]} ${styles["default"]}`}>
+        <div className={styles["group"]}>
+          <label>First Name</label>
+          <span>{user?.realName?.firstName}</span>
+        </div>
+        <div className={styles["group"]}>
+          <label>Last Name</label>
+          <span>{user?.realName?.lastName}</span>
+        </div>
+        <div className={styles["group"]}>
+          <label>Email</label>
+          <span>{user?.email}</span>
+        </div>
+      </div>
+      <div className={`${styles["row"]} ${styles["form"]}`}>
+        <form onReset={handleFormReset} onSubmit={handleFormSubmit}>
+          <div className={styles["group"]}>
+            <label>Display Name</label>
+            <TextInput
+              id="displayName"
+              value={newProfile?.displayName}
+              onChange={handleProfileChange}
+              placeholder={user?.displayName}
+            />
+          </div>
+          <div className={styles["group"]}>
+            <label>Contact Number</label>
+            <TextInput
+              id="contact"
+              value={newProfile?.contact}
+              onChange={handleProfileChange}
+              placeholder={user?.contact}
+            />
+          </div>
+          <div className={styles["group"]}>
+            <label>Bio</label>
+            <TextArea
+              id="bio"
+              value={newProfile?.bio}
+              onChange={handleProfileChange}
+              placeholder={user?.bio}
+              fullHeight
+            />
+          </div>
+          <div className={styles["form-controls"]}>
+            <IconButton
+              icon={<RedoIcon />}
+              type="reset"
+              variant="outlined"
+              rounded
+            />
+            <Button
+              type="submit"
+              label="save changes"
+              variant="contained"
+              rounded
+              fullWidth
+            />
+          </div>
+        </form>
+      </div>
+    </div>
   );
 };
 
