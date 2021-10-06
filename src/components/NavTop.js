598314e431fdd6b337d6a724c651f723f7ec2d81
John Paul Ong <jpong5202@gmail.com>
2021-10-06 17:11:28 +0800

    Add hamburger button functionality w/ main menu

--- src/components/NavTop.js
+++ src/components/NavTop.js
@@ -5,11 +5,17 @@ import IconButton from "./IconButton";
 import BarsIcon from "./icons/BarsIcon";
 import SearchIcon from "./icons/SearchIcon";
 import MainMenu from "./MainMenu";
+import { useState } from "react";
+import { AnimatePresence } from "framer-motion";
 
 const NavTop = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
   return (
     <nav className={`${styles["nav"]} ${styles["top"]}`}>
-      <IconButton icon={<BarsIcon />} />
+      <IconButton
+        icon={<BarsIcon />}
+        onClick={() => setMenuOpen((prev) => !prev)}
+      />
       <Link href="/">
         <a>
           <Image
@@ -22,7 +28,10 @@ const NavTop = () => {
         </a>
       </Link>
       <IconButton icon={<SearchIcon />} />
-      <MainMenu />
+
+      <AnimatePresence>
+        {isMenuOpen && <MainMenu key={isMenuOpen} isOpen={isMenuOpen} />}
+      </AnimatePresence>
     </nav>
   );
 };
