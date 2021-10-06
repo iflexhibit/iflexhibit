66c142d47649b7e59ffc1a7b12bc0100339c10d3
John Paul Ong <jpong5202@gmail.com>
2021-10-06 17:11:06 +0800

    Add MainMenu new design and animation

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,37 +1,69 @@
 import styles from "styles/MainMenu.module.scss";
+import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import Button from "./Button";
-import IconButton from "./IconButton";
-import TimesIcon from "./icons/TimesIcon";
+import UserStatus from "./UserStatus";
+import UserIcon from "./icons/UserIcon";
+import CogIcon from "./icons/CogIcon";
+import BlocksIcon from "./icons/BlocksIcon";
+import FlagIcon from "./icons/FlagIcon";
+import { motion } from "framer-motion";
 
 const MainMenu = () => {
+  const [{ username, usertype }] = useState({
+    username: "nikkieyabs",
+    usertype: "administrator",
+  });
+  const [links] = useState([
+    { href: "/profile", label: "My Profile", icon: <UserIcon /> },
+    { href: "/account", label: "Account Settings", icon: <CogIcon /> },
+    { href: "/", label: "System Dashboard", icon: <BlocksIcon /> },
+    { href: "/", label: "Report", icon: <FlagIcon /> },
+  ]);
   return (
     <div className={styles["mainmenu"]}>
-      <div className={styles["menu"]}>
+      <motion.div
+        key="underlay"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 1 }}
+        className={styles["underlay"]}
+      ></motion.div>
+      <motion.div
+        key="menu"
+        initial={{ height: "0%" }}
+        animate={{ height: "min-content" }}
+        exit={{ height: "0%", opacity: [1, 1, 0] }}
+        transition={{ duration: 0.5, ease: [0.16, 1, 0.3, 1] }}
+        className={styles["menu"]}
+      >
         <div className={styles["top"]}>
-          <Link href="/">
-            <a>
-              <Image
-                src="/assets/logos/lettermark.png"
-                layout="fill"
-                objectFit="contain"
-              />
-            </a>
-          </Link>
+          <div className={styles["avatar"]}>
+            <Image src="/assets/temp/hmm.jpg" layout="fill" objectFit="cover" />
+          </div>
+          <div className={styles["info"]}>
+            <div className={styles["user"]}>{username}</div>
+            <UserStatus status={usertype} color="green" />
+          </div>
+        </div>
+        <div className={styles["links"]}>
+          {links.map((link, index) => (
+            <div key={index} className={styles["link"]}>
+              <Link href={link.href}>
+                <a>
+                  {link.icon}
+                  <span>{link.label}</span>
+                </a>
+              </Link>
+            </div>
+          ))}
         </div>
         <div className={styles["bottom"]}>
-          <Button
-            href="/"
-            variant="contained"
-            label="Sign in to iflexhibit"
-            fullWidth
-          />
+          <Button label="sign out" variant="contained" fullWidth />
         </div>
-        <div className={styles["exit"]}>
-          <IconButton icon={<TimesIcon />} variant="outlined" />
-        </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
