1de9e4518754068ba6275b23ccc685684602599f
John Paul Ong <jpong5202@gmail.com>
2021-10-16 17:21:34 +0800

    Add set headers

--- src/pages/api/users/user/index.js
+++ src/pages/api/users/user/index.js
@@ -5,6 +5,7 @@ export default async function handler(req, res) {
     const response = await axios.get(process.env.API_URL + "/api/users/user", {
       validateStatus: (status) => status < 400,
       withCredentials: true,
+      headers:req.headers
     });
     return res.status(response.status).json(response.data);
   } catch (error) {
