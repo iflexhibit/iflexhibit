d59f969d4a38a11a44f678c04ea3baee6101e4d7
John Paul Ong <jpong5202@gmail.com>
2021-10-16 15:41:15 +0800

    Update error handling

--- src/pages/api/users/user/index.js
+++ src/pages/api/users/user/index.js
@@ -9,6 +9,6 @@ export default async function handler(req, res) {
     });
     return res.status(response.status).json(response.data);
   } catch (error) {
-    return res.status(error.response.status).json(error.response.data);
+    return res.end();
   }
 }
