b6159a6969ca8bcd3836f6a268196c44e0a3f987
John Paul Ong <jpong5202@gmail.com>
2021-10-16 14:35:41 +0800

    Add test logger

--- src/pages/api/users/user/index.js
+++ src/pages/api/users/user/index.js
@@ -9,6 +9,7 @@ export default async function handler(req, res) {
     });
     return res.status(response.status).json(response.data);
   } catch (error) {
+    console.log(error);
     return res.status(error.response.status).json(error.response.data);
   }
 }
