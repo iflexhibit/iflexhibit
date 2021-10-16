093116e9e9997078afdb78707291db35c8964ab9
John Paul Ong <jpong5202@gmail.com>
2021-10-16 15:28:40 +0800

    Remove logging

--- src/pages/api/users/user/index.js
+++ src/pages/api/users/user/index.js
@@ -5,11 +5,10 @@ export default async function handler(req, res) {
     const response = await axios.get(process.env.API_URL + "/api/users/user", {
       headers: req.headers,
       withCredentials: true,
-      validateStatus: (status) => status < 400,
+      validateStatus: (status) => status <= 400,
     });
     return res.status(response.status).json(response.data);
   } catch (error) {
-    console.log(error);
     return res.status(error.response.status).json(error.response.data);
   }
 }
