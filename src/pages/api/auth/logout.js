9a49a6eb96b787b64c4a6826651f335a96884eec
John Paul Ong <jpong5202@gmail.com>
2021-11-07 19:35:44 +0800

    Change sign out redirect to sign in page

--- src/pages/api/auth/logout.js
+++ src/pages/api/auth/logout.js
@@ -3,5 +3,5 @@ import Cookies from "cookies";
 export default async function handler(req, res) {
   const cookies = new Cookies(req, res);
   cookies.set("token");
-  res.redirect("/");
+  res.redirect("/login");
 }
