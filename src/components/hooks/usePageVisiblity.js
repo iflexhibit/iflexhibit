e666d2217ebc8388a60155cf05ca08cae227fe73
John Paul Ong <jpong5202@gmail.com>
2022-02-02 19:21:17 +0800

    Optimize desktop elements and flow

---
+++ src/components/hooks/usePageVisiblity.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+
+export function getBrowserVisibilityProp() {
+  if (!document) return;
+  if (typeof document.hidden !== "undefined") {
+    return "visibilitychange";
+  } else if (typeof document.msHidden !== "undefined") {
+    return "msvisibilitychange";
+  } else if (typeof document.webkitHidden !== "undefined") {
+    return "webkitvisibilitychange";
+  }
+}
+
+export function getBrowserDocumentHiddenProp() {
+  if (!document) return;
+  if (typeof document.hidden !== "undefined") {
+    return "hidden";
+  } else if (typeof document.msHidden !== "undefined") {
+    return "msHidden";
+  } else if (typeof document.webkitHidden !== "undefined") {
+    return "webkitHidden";
+  }
+}
+
+export function getIsDocumentHidden() {
+  if (typeof document !== "undefined")
+    return !document[getBrowserDocumentHiddenProp()];
+}
+
+export function usePageVisibility() {
+  const [isVisible, setIsVisible] = useState(getIsDocumentHidden());
+  const onVisibilityChange = () => setIsVisible(getIsDocumentHidden());
+
+  useEffect(() => {
+    const visibilityChange = getBrowserVisibilityProp();
+
+    document.addEventListener(visibilityChange, onVisibilityChange, false);
+
+    return () => {
+      document.removeEventListener(visibilityChange, onVisibilityChange);
+    };
+  });
+
+  return isVisible;
+}
--- src/components/layouts/LoginLayout/LoginLayout.js
+++ src/components/layouts/LoginLayout/LoginLayout.js
@@ -4,8 +4,28 @@ import { AnimatedBackground } from "./AnimatedBackground";
 import { NoticeSection } from "./NoticeSection";
 import { LoginControls } from "./LoginControls";
 import { LogoSection } from "./LogoSection";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useRouter } from "next/router";
+import { authUser } from "redux/actions/authAction";
+import { usePageVisibility } from "components/hooks/usePageVisiblity";
 
 const LoginLayout = () => {
+  const { isAuthenticated } = useSelector((state) => state.auth);
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const isVisible = usePageVisibility();
+  useEffect(() => {
+    if (
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
+    )
+      return;
+    if (isAuthenticated) return router.replace("/");
+    if (isVisible) return dispatch(authUser());
+  }, [isAuthenticated, isVisible]);
+
   return (
     <Layout
       title="Join the Community | iFlexhibit"
